1b7aae3b712a103cc7a915418e2d1f65cccfc8e1
Aditya Raj <157242162+AdityaR0@users.noreply.github.com>
2025-09-17 17:52:25 +0530

    Update ChatbotPage.js

--- src/components/ChatbotPage.js
+++ src/components/ChatbotPage.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function ChatbotPage() {
   return (
-    // <div style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+    <div style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
       <iframe
         src={process.env.REACT_APP_API_URL}
         title="Agri Chatbot"
